c163f50cddfe0dbf9ac09e12c43018fbea32a392
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2025-01-17 09:26:08 +0800

    Added layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,7 @@
+<script setup>
+
+</script>
+
 <template>
-    <div>
-        <h1 class="text-3xl font-bold underline">Hello World</h1>
-    </div>
+    <h1 class="text-3xl font-bold underline">Hello World</h1>
 </template>
