faf4d7d28c07c432fad5d16af98a590213df9e8e
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2025-01-18 19:02:37 +0800

    Added preserve scroll

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,8 +1,10 @@
 <script setup>
-    import {Head} from '@inertiajs/vue3';
+    import {Head, Link} from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head :title="$page.component"/>
     <h1 class="text-3xl font-bold underline">{{ $page.component }}</h1>
+    <Link class="mt-[60000px] block" :href="route('home')" preserve-scroll>Refresh</Link>
+
 </template>
