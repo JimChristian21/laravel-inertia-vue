f23979c9b12b2077a99c2494c0a0b3569b2c1202
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2025-01-18 18:49:53 +0800

    Added ziggy

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return inertia('About', [
         'user' => 'Mike'
     ]);
-});
+})->name('about');
