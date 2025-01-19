43a8e202c6b83e639588d48921a32c0ee8ebeaa6
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2025-01-19 09:47:48 +0800

    Register user and used useForm in inertia

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,8 +1,7 @@
 <script setup>
-    import {Head} from '@inertiajs/vue3';
-    import { reactive } from 'vue';
+    import {Head, useForm} from '@inertiajs/vue3';
 
-    const form = reactive({
+    const form = useForm({
         name: null,
         email: null,
         password: null,
@@ -10,8 +9,9 @@
     });
 
     const submit = () => {
-        console.log(form);
-        console.log('submit');
+       form.post(route('register'), {
+        onError: () => form.reset('password', 'password_confirmation')
+       });
     }
 </script>
 
@@ -25,25 +25,29 @@
             <div class="mb-6">
                 <label>Name</label>
                 <input type="text" v-model="form.name"/>
+                <small>{{ form.errors.name }}</small>
             </div>
             <div class="mb-6">
                 <label>Email</label>
                 <input type="text" v-model="form.email"/>
+                <small>{{ form.errors.email }}</small>
             </div>
             <div class="mb-6">
                 <label>Password</label>
                 <input type="password" v-model="form.password"/>
+                <small>{{ form.errors.password }}</small>
             </div>
             <div class="mb-6">
                 <label>Confirm Password</label>
                 <input type="password" v-model="form.password_confirmation"/>
+                <small>{{ form.errors.password_confirmation }}</small>
             </div>
             <div>
                 <p class="text-slate-600 mb-2">
                     Alreay a user? 
                     <a href="#" class="text-link">Login</a>
                 </p>
-                <button class="primary-btn">Register</button>
+                <button class="primary-btn" :disabled="form.processing">Register</button>
             </div>
         </form>
     </div>
