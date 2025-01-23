19167c9d3d1e3305712594f419a22f42574968e3
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2025-01-24 01:16:59 +0800

    Added pagination

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,12 +1,46 @@
 <script setup>
-    import {Head} from '@inertiajs/vue3';
+    import {Head, Link} from '@inertiajs/vue3';
+    import PaginationLinks from './Components/PaginationLinks.vue';
 
     defineProps({
         users: Object
     });
+
+    const getDate = (date) => {
+        return new Date(date).toLocaleDateString('en-us', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        })
+    }
 </script>
 
 <template>
     <Head :title="$page.component"/>
-    <h1>{{ $page.component }} Page</h1>
+    <h1> Users </h1>
+    <div>
+        <table>
+            <thead>
+                <tr class="bg-slate-300">
+                    <th>Avatar</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Registarion</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users.data" :key="user.id">
+                   <td>
+                        <img :src="user.avatar ? ('storage/' + user.avatar) : '#'" class="avatar" alt="avatar"/>
+                   </td>
+                   <td>{{ user.name }}</td> 
+                   <td>{{ user.email }}</td> 
+                   <td>{{ getDate(user.created_at) }}</td> 
+                </tr>
+            </tbody>
+        </table>
+        <div>
+            <PaginationLinks :paginator="users"/>
+        </div>
+    </div>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Home', ['users' => User::all()])->name('home');
+Route::inertia('/', 'Home', ['users' => User::paginate(5)])->name('home');
 
 Route::middleware('auth')->group(function() {
 
